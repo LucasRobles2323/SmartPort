fcd4f301a03f9bb933c0d11c4ebec1b06ae8e405
Lucas <lucasrobles2323@gmail.com>
2024-06-17 20:53:28 -0400

    Inicio Sesión casi hecho
    Solo falta que inicie correctamente si se ingresan los datos correctos.

--- SmartPort-FrontEnd/src/app/tab3/tab3.module.ts
+++ SmartPort-FrontEnd/src/app/tab3/tab3.module.ts
@@ -1,12 +1,12 @@
 import { IonicModule } from '@ionic/angular';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Tab3Page } from './tab3.page';
-import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
 
 import { Tab3PageRoutingModule } from './tab3-routing.module';
 import { SharedModule } from '../shared/shared.module';
+
 
 @NgModule({
   imports: [
@@ -14,7 +14,7 @@ import { SharedModule } from '../shared/shared.module';
     CommonModule,
     FormsModule,
     SharedModule,
-    ExploreContainerComponentModule,
+    ReactiveFormsModule,
     Tab3PageRoutingModule
   ],
   declarations: [Tab3Page]
--- SmartPort-FrontEnd/src/app/tab3/tab3.page.ts
+++ SmartPort-FrontEnd/src/app/tab3/tab3.page.ts
@@ -1,12 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { FormError, mensajesErr } from '../misc/form-errors';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page {
+export class Tab3Page implements OnInit  {
+  loginForm: FormGroup;
 
-  constructor() {}
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(20)]]
+    });
+  }
+
+  ngOnInit() {
+    // Verificar si ya inicio sesión
+  }
+
+  get btnText() {
+    return this.loginForm.valid ? 'Iniciar Sesión' : 'Iniciar Sesión';
+  }
+
+  // Método que se ejecuta al enviar el formulario
+  onSubmit() {
+    console.log(this.loginForm.value);
+  }
+
+  /**
+   * Función que retorna el mensaje de error asociado a un campo del formulario
+   * @param campo Campo del formulario (controlado)
+   * @returns Mensaje de error asociado al campo, si es que existe
+   */
+  formError(campo: string): string | null {
+    const control = this.loginForm.get(campo);
+
+    if (control && control.errors) {
+      const error: FormError = Object.keys(this.loginForm.get(campo)!.errors!)[0] as FormError;
+      return mensajesErr[error];
+    }
+
+    return null;
+  }
 
 }
