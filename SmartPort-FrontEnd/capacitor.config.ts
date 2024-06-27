7021474f38c0054f1b3ecf06baef2664f25cab24
Lucas <lucasrobles2323@gmail.com>
2024-06-26 22:10:45 -0400

    Borrando antiguo

--- SmartPort-FrontEnd/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'SmartPort-FrontEnd',
-  webDir: 'www'
-};
-
-export default config;
