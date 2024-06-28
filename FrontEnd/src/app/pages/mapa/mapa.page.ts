956390d06d9ef43105e624984577a93b2bf9b75c
Gerald Espinoza <75864878+GeraldEspi@users.noreply.github.com>
2024-06-27 21:36:28 -0400

    Mapa hecho

--- FrontEnd/src/app/pages/mapa/mapa.page.ts
+++ FrontEnd/src/app/pages/mapa/mapa.page.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-mapa',
   templateUrl: './mapa.page.html',
   styleUrls: ['./mapa.page.scss'],
 })
-export class MapaPage implements OnInit {
+export class MapaPage {
+  losbuses: boolean = false;
+  constructor() {}
 
-  constructor() { }
+  toggleChanged(event: CustomEvent) {
+    const isChecked = event.detail.checked;
+    if (isChecked) {
+      console.log('El toggle está activado.');
+      this.losbuses = true;
 
-  ngOnInit() {
+    } else {
+      console.log('El toggle está desactivado.');
+      this.losbuses = false;
+
+    }
   }
-
 }
